6e200a5ad0ecc2ca445013cf96b63bcfb19f033d
VinhLL <115599544+VinhLL@users.noreply.github.com>
2022-12-10 07:00:24 +0700

    Add files via upload

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+typedef vector < vector < char > > State;
+const int SIZE = 3;
+// Function: Render game
+// Input: Current game state
+// Output: Print current game state to screen
+void renderGame(State state) {
+  cout << " -------------" << endl;
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      cout << " | " << state[i][j];
+    }
+    cout << " |" << endl << " -------------" << endl;
+  }
+}
+
+// Function: Detect next player (X or O)
+// Input: Current game state
+// Output: X or O
+char getNextPlayer(State s) {
+  int countX = 0, countO = 0;
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (s[i][j] == 'X') countX++;
+      else if (s[i][j] == 'O') countO++;
+    }
+  }
+  if (countX == countO) return 'X';
+  else return 'O';
+}
+
+// Function: Change current game state
+// Input: Current game state, move's coordinate (i and j), current player (X or O)
+// Output: New game state after making the move
+State play(State s, int i, int j, char player) {
+  State newState = State(3, vector < char > (3, ' '));
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      newState[i][j] = s[i][j];
+    }
+  }
+  newState[i][j] = player;
+  return newState;
+}
+
+// Function: Find possible next game states
+// Input: Current state
+// Output: Next game states
+vector < State > getNextStates(State s) {
+  char nextPlayer = getNextPlayer(s);
+  vector < State > states;
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (s[i][j] == ' ') {
+        State nextState = play(s, i, j, nextPlayer);
+        states.push_back(nextState);
+      }
+    }
+  }
+  return states;
+}
+
+// Functine: Check if current state is final state
+// Input: Current state
+// Output: True (if current state is final state)/ False (if current state is not final state)
+bool isFinalState(State s) {
+  // check rows
+  for (int i = 0; i < SIZE; i++)
+    if (s[i][0] == s[i][1] && s[i][0] == s[i][2] && s[i][0] != ' ') return true;
+  // check cols
+  for (int i = 0; i < SIZE; i++)
+    if (s[0][i] == s[1][i] && s[0][i] == s[2][i] && s[0][i] != ' ') return true;
+  // check diagonals
+  if (s[0][0] == s[1][1] && s[0][0] == s[2][2] && s[0][0] != ' ') return true;
+  if (s[0][2] == s[1][1] && s[0][2] == s[2][0] && s[0][2] != ' ') return true;
+  // check full
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (s[i][j] == ' ') return false;
+    }
+  }
+  return true;
+}
+
+// Function: Get final state's score
+// Input: State (final)
+// Output: Score (1 if X wins, -1 if O wins, 0 if draw)
+int getScoreFinalState(State s) {
+  // check rows
+  for (int i = 0; i < SIZE; i++)
+    if (s[i][0] == s[i][1] && s[i][0] == s[i][2] && s[i][0] != ' ')
+      return s[i][0] == 'X' ? 1 : -1;
+  // check cols
+  for (int i = 0; i < SIZE; i++)
+    if (s[0][i] == s[1][i] && s[0][i] == s[2][i] && s[0][i] != ' ')
+      return s[0][i] == 'X' ? 1 : -1;
+  if (s[0][0] == s[1][1] && s[0][0] == s[2][2] && s[0][0] != ' ')
+    return s[0][0] == 'X' ? 1 : -1;
+  if (s[0][2] == s[1][1] && s[0][2] == s[2][0] && s[0][2] != ' ')
+    return s[0][2] == 'X' ? 1 : -1;;
+  return 0;
+}
+
+// Function: Get score of current state
+// Input: Current state
+// Output: Best score can be reached from current state
+pair < int, State > getScore(State s) {
+  if (isFinalState(s)) {
+    return make_pair(getScoreFinalState(s), s);
+  }
+  char player = getNextPlayer(s);
+  vector < State > states = getNextStates(s);
+  int bestScore = -10;
+  State bestState;
+  for (State ns: states) {
+    pair < int, State > p = getScore(ns);
+    int score = p.first;
+    if (player == 'X') {
+      if (bestScore < score || bestScore == -10) {
+        bestScore = score;
+        bestState = ns;
+      }
+    } else {
+      if (bestScore > score || bestScore == -10) {
+        bestScore = score;
+        bestState = ns;
+      }
+    }
+  }
+  return make_pair(bestScore, bestState);
+}
+
+// Function: Get coordinate of next computer's move
+// Input: Current state (at computer's turn)
+// Output: Next computer move's coordinate
+pair < int, int > getComputerPlay(State s) {
+  pair < int, State > p = getScore(s);
+  int score = p.first;
+  State newState = p.second;
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      if (newState[i][j] != s[i][j]) return make_pair(i, j);
+    }
+  }
+  return make_pair(-1, -1);
+}
+
+int main() {
+  State s = State(3, vector < char > (3, ' '));
+  char humanPlayer = ' ', currentPlayer = 'X';
+  while (humanPlayer != 'X' && humanPlayer != 'O') {
+    cout << "Choose your player (X or O): ";
+    cin >> humanPlayer;
+    if (humanPlayer != 'X' && humanPlayer != 'O') {
+      cout << "Invalid choice! Choose again." << endl;
+    }
+  }
+  cout << "You are " << humanPlayer << " player" << endl;
+  renderGame(s);
+  while (true) {
+    int i = -1, j = -1;
+    if (humanPlayer == currentPlayer) {
+      cout << "Enter your play: ";
+      cin >> i >> j;
+      i--;
+      j--;
+      if ((i < 0 || i > 2) || (j < 0 || j > 2)) {
+        cout << "Invalid move! Play it again." << endl;
+        continue;
+      }
+      if (s[i][j] != ' ') {
+        cout << "Invalid move! Play it again." << endl;
+        continue;
+      }
+      cout << "You play (" << i + 1 << ", " << j + 1 << ")" << endl;
+
+    } else {
+      pair < int, int > move = getComputerPlay(s);
+      i = move.first;
+      j = move.second;
+      cout << "I play (" << i + 1 << ", " << j + 1 << ")" << endl;
+    }
+    s = play(s, i, j, currentPlayer);
+    renderGame(s);
+
+    currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+    if (isFinalState(s)) {
+      int score = getScoreFinalState(s);
+      if (score > 0) {
+        cout << "X won" << endl;
+      } else if (score < 0) {
+        cout << "X lost" << endl;
+      } else {
+        cout << "A draw" << endl;
+      }
+      break;
+    }
+  }
+    system("pause");
+  return 0;
+
+}
